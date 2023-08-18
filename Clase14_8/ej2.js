9885795b07b7753055ad267c9536f10630f638f9
RuizFrancisco <francisco02rc@gmail.com>
2023-08-17 22:08:05 -0300

    17/8

---
+++ Clase14_8/ej2.js
@@ -0,0 +1,15 @@
+let input = [
+    { name: "John", age: 21, city: "New York" },
+    { name: "Mike", age: 28, city: "Moscow" },
+    { name: "Danny", age: 30, city: "London" },
+    { name: "Lisa", age: 26, city: "Paris" },
+    { name: "Sophie", age: 19, city: "Berlin" },
+];
+
+const age = (input) =>
+    input
+        .filter((item) => item.age > 20 && item.age < 29)
+        .map((item) => item.name);
+
+
+console.log(age(input));
---
+++ Clase14_8/ej5.js
@@ -0,0 +1,11 @@
+let arrayClass = [0, 1, 1, 1, 0, 0];
+const min = 3;
+
+console.log("¿Se hace la clase?");
+const assist = (arrayClass, min) =>
+    arrayClass.filter((item) => item <= 0).length >= min
+    ? "NO"
+    : "YES";
+
+
+console.log(assist(arrayClass, min));
