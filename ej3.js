8ea4637e6f4ed88399c5009af1dc56af23aea07c
Francisco Rafael Ruiz <116575917+RuizFrancisco@users.noreply.github.com>
2023-08-17 22:42:19 -0300

    Delete ej3.js

--- ej3.js
+++
@@ -1,9 +0,0 @@
-let input = [
-    { name: "Argentina", population: 45810000},
-    { name: "Chile", population: 19493184},
-    { name: "Brasil", population:  215586573},
-];
-
-const arrayPopulation = input.sort((a, b) => b.population - a.population); 
-
-console.log(arrayPopulation);
